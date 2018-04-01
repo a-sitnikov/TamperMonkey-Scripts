b760814ff3c48663824b070319b46f1e76e8b3d2
Anatoliy Sitnikov <a.sitnikov@outlook.com>
2018-04-01 19:24:08 +0300

    Create ixbt.js

---
+++ ixbt.js
@@ -0,0 +1,16 @@
+/// ==UserScript==
+// @name         ixbt.com
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  change style of comments
+// @author       a.sitnikov
+// @match        www.ixbt.com/*
+// @downloadURL  https://cdn.jsdelivr.net/gh/a-sitnikov/TamperMonkey-Scripts@latest/ixbt.js
+// @updateURL    https://cdn.jsdelivr.net/gh/a-sitnikov/TamperMonkey-Scripts@latest/ixbt.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //console.log(1);
+})();
